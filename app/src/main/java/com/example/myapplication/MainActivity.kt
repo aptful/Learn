51bb79c25284334c3063f54a1ccb394b0ef04c94
Hitominnn <libre.chat.1103@gmail.com>
2021-12-31 20:40:53 +0900

    変更

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.signup1)
+
+        val signupTwo = findViewById<Button>(R.id.signupTwo)    //新規登録画面1の【次へ】
+
+        signupTwo.setOnClickListener {
+            //Intentオブジェクト生成、遷移画面定義
+            val nextIntent = Intent(this, IntentTestActivity::class.java)
+            //次のActivity実行
+            startActivity(nextIntent)
+        }
     }
+
 }
