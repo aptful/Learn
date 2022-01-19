845edce6319232ede264a26d5eff58517355184f
miiiiiiinami <minami.t.627@au.com>
2022-01-19 16:17:45 +0900

    新規登録画面3への画面遷移の条件追加

--- app/src/main/java/com/example/myapplication/signupTwo.kt
+++ app/src/main/java/com/example/myapplication/signupTwo.kt
@@ -16,14 +16,6 @@ class signupTwo : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signup2)
 
-        //新規登録画面2ボタン
-        val nextButton : Button = findViewById(R.id.nextButton)
-
-        //新規登録画面3に遷移
-        nextButton.setOnClickListener {
-            val intent = Intent(this, signupThree::class.java)
-            startActivity(intent)
-        }
 
         //戻るボタン
         val backButton : ImageView = findViewById(R.id.backButton)
@@ -79,114 +71,147 @@ class signupTwo : AppCompatActivity() {
         //ボタンの動き
         //INVISIBLE:隠す　VISIBLE:出す
 
+        var count : Int = 0
+
         //追加ボタン1の動き
         addButton1.setOnClickListener{
             addButton1.visibility = View.INVISIBLE
             addedButton1.visibility = View.VISIBLE
+            count++
         }
         //追加済ボタン1の動き
         addedButton1.setOnClickListener{
             addedButton1.visibility = View.INVISIBLE
             addButton1.visibility = View.VISIBLE
+            count--
         }
 
         //追加ボタン2の動き
         addButton2.setOnClickListener{
             addButton2.visibility = View.INVISIBLE
             addedButton2.visibility = View.VISIBLE
+            count++
         }
         //追加済ボタン2の動き
         addedButton2.setOnClickListener{
             addedButton2.visibility = View.INVISIBLE
             addButton2.visibility = View.VISIBLE
+            count--
         }
 
         //追加ボタン3の動き
         addButton3.setOnClickListener{
             addButton3.visibility = View.INVISIBLE
             addedButton3.visibility = View.VISIBLE
+            count++
         }
         //追加済ボタン3の動き
         addedButton3.setOnClickListener{
             addedButton3.visibility = View.INVISIBLE
             addButton3.visibility = View.VISIBLE
+            count--
         }
 
         //追加ボタン4の動き
         addButton4.setOnClickListener{
             addButton4.visibility = View.INVISIBLE
             addedButton4.visibility = View.VISIBLE
+            count++
         }
         //追加済ボタン4の動き
         addedButton4.setOnClickListener{
             addedButton4.visibility = View.INVISIBLE
             addButton4.visibility = View.VISIBLE
+            count--
         }
 
         //追加ボタン5の動き
         addButton1.setOnClickListener{
             addButton5.visibility = View.INVISIBLE
             addedButton5.visibility = View.VISIBLE
+            count++
         }
         //追加済ボタン5の動き
         addedButton5.setOnClickListener{
             addedButton5.visibility = View.INVISIBLE
             addButton5.visibility = View.VISIBLE
+            count--
         }
 
         //追加ボタン6の動き
         addButton6.setOnClickListener{
             addButton6.visibility = View.INVISIBLE
             addedButton6.visibility = View.VISIBLE
+            count++
         }
         //追加済ボタン6の動き
         addedButton6.setOnClickListener{
             addedButton6.visibility = View.INVISIBLE
             addButton6.visibility = View.VISIBLE
+            count--
         }
 
         //追加ボタン7の動き
         addButton7.setOnClickListener{
             addButton7.visibility = View.INVISIBLE
             addedButton7.visibility = View.VISIBLE
+            count++
         }
         //追加済ボタン7の動き
         addedButton7.setOnClickListener{
             addedButton7.visibility = View.INVISIBLE
             addButton7.visibility = View.VISIBLE
+            count--
         }
 
         //追加ボタン8の動き
         addButton8.setOnClickListener{
             addButton8.visibility = View.INVISIBLE
             addedButton8.visibility = View.VISIBLE
+            count++
         }
         //追加済ボタン8の動き
         addedButton8.setOnClickListener{
             addedButton8.visibility = View.INVISIBLE
             addButton8.visibility = View.VISIBLE
+            count--
         }
 
         //追加ボタン9の動き
         addButton9.setOnClickListener{
             addButton9.visibility = View.INVISIBLE
             addedButton9.visibility = View.VISIBLE
+            count++
         }
         //追加済ボタン9の動き
         addedButton1.setOnClickListener{
             addedButton9.visibility = View.INVISIBLE
             addButton9.visibility = View.VISIBLE
+            count--
         }
 
         //追加ボタン10の動き
         addButton10.setOnClickListener{
             addButton10.visibility = View.INVISIBLE
             addedButton10.visibility = View.VISIBLE
+            count++
         }
         //追加済ボタン10の動き
         addedButton10.setOnClickListener{
             addedButton10.visibility = View.INVISIBLE
             addButton10.visibility = View.VISIBLE
+            count--
+        }
+
+        //次へボタン
+        val nextButton : Button = findViewById(R.id.nextButton)
+
+        //新規登録画面3に遷移
+        nextButton.setOnClickListener {
+            if (count > 0) {
+                val intent = Intent(this, signupThree::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
