e290f8cf815031cfd13cb1e20f40e2956b361400
Hitominnn <libre.chat.1103@gmail.com>
2022-01-20 19:32:52 +0900

    生年月日の制限（上限）を追加

--- app/src/main/java/com/example/myapplication/signupOne.kt
+++ app/src/main/java/com/example/myapplication/signupOne.kt
@@ -1,15 +1,21 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import androidx.annotation.RequiresApi
+import java.time.LocalDate
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 //作成者：綾部，井口
 
 class signupOne : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signup1)
@@ -23,6 +29,11 @@ class signupOne : AppCompatActivity() {
         //誕生日入力欄を取得してint型に変換
         val textDayEdit = findViewById<EditText>(R.id.dayEdit)
 
+        //今日の日付を取得
+        val dateNow: LocalDate = LocalDate.now()
+        //今日の日付をyyyymmddに変換
+        val dateSum = dateNow.year * 10000 + dateNow.monthValue * 100 + dateNow.dayOfMonth
+
         //新規登録画面1の次へ
         val nextButton: Button = findViewById(R.id.nextButton)
 
@@ -32,7 +43,10 @@ class signupOne : AppCompatActivity() {
             //入力制限．パスワードが6文字以上，誕生年が4文字以上，誕生月日が1文字以上
             if (textPasswordEdit.length() > 5 && textYearEdit.length() > 3 && textMonthEdit.length() > 0 && textDayEdit.length() > 0) {
                 //誕生年が1901年以上
-                if (textYearEdit.getText().toString().toInt() > 1900) {
+                //（今日の日付）-（誕生年*10000 + 誕生月*100 + 誕生日 + 1）の結果が正
+                //生年月日をyyyymmddに変換
+                val birth = textYearEdit.getText().toString().toInt() * 10000 + textMonthEdit.getText().toString().toInt() * 100 + textDayEdit.getText().toString().toInt() - 1
+                if (textYearEdit.getText().toString().toInt() > 1900 && (dateSum - birth) > 0) {
                     //誕生月によって日にちの制限．うるう年を想定して2月はどの年でも29日を入力可としている
                     when (textMonthEdit.getText().toString().toInt()) {
                         1, 3, 5, 7, 8, 10, 12 -> {
