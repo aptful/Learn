8056336ac5271e9d87f410ecb672c2ce7803a10c
Hitominnn <libre.chat.1103@gmail.com>
2022-01-20 18:53:19 +0900

    生年月日の制限（下限）を追加

--- app/src/main/java/com/example/myapplication/signupOne.kt
+++ app/src/main/java/com/example/myapplication/signupOne.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
-import android.view.Gravity
 
 //作成者：綾部，井口
 
@@ -21,44 +20,85 @@ class signupOne : AppCompatActivity() {
         val textYearEdit = findViewById<EditText>(R.id.yearEdit)
         //誕生月入力欄
         val textMonthEdit = findViewById<EditText>(R.id.monthEdit)
-        //誕生日入力欄
+        //誕生日入力欄を取得してint型に変換
         val textDayEdit = findViewById<EditText>(R.id.dayEdit)
+
         //新規登録画面1の次へ
-        val nextButton :Button =findViewById(R.id.nextButton)
+        val nextButton: Button = findViewById(R.id.nextButton)
 
         //新規登録画面2に遷移
         nextButton.setOnClickListener {
             val intent = Intent(this, signupTwo::class.java)
-            if(textPasswordEdit.length() > 5 && textYearEdit.length() > 3 && textMonthEdit.length() > 0 && textDayEdit.length() > 0){
-                //値の引き渡し
-                //パスワード
-                intent.putExtra("passwordKey", textPasswordEdit.getText().toString())
-                //誕生年
-                intent.putExtra("yearKey", textYearEdit.getText().toString())
-                //誕生月
-                intent.putExtra("monthKey", textMonthEdit.getText().toString())
-                //誕生日
-                intent.putExtra("dayKey", textDayEdit.getText().toString())
-                startActivity(intent)
+            //入力制限．パスワードが6文字以上，誕生年が4文字以上，誕生月日が1文字以上
+            if (textPasswordEdit.length() > 5 && textYearEdit.length() > 3 && textMonthEdit.length() > 0 && textDayEdit.length() > 0) {
+                //誕生年が1901年以上
+                if (textYearEdit.getText().toString().toInt() > 1900) {
+                    //誕生月によって日にちの制限．うるう年を想定して2月はどの年でも29日を入力可としている
+                    when (textMonthEdit.getText().toString().toInt()) {
+                        1, 3, 5, 7, 8, 10, 12 -> {
+                            if (32 > textDayEdit.getText().toString().toInt()) {
+                                //値の引き渡し
+                                //パスワード
+                                intent.putExtra("passwordKey", textPasswordEdit.getText().toString())
+                                //誕生年
+                                intent.putExtra("yearKey", textYearEdit.getText().toString())
+                                //誕生月
+                                intent.putExtra("monthKey", textMonthEdit.getText().toString())
+                                //誕生日
+                                intent.putExtra("dayKey", textDayEdit.getText().toString())
+                                startActivity(intent)
+                            }
+                        }
+                        4, 6, 9, 11-> {
+                            if (31 > textDayEdit.getText().toString().toInt()) {
+                                //値の引き渡し
+                                //パスワード
+                                intent.putExtra("passwordKey", textPasswordEdit.getText().toString())
+                                //誕生年
+                                intent.putExtra("yearKey", textYearEdit.getText().toString())
+                                //誕生月
+                                intent.putExtra("monthKey", textMonthEdit.getText().toString())
+                                //誕生日
+                                intent.putExtra("dayKey", textDayEdit.getText().toString())
+                                startActivity(intent)
+                            }
+                        }
+                        2 -> {
+                            if (30 > textDayEdit.getText().toString().toInt()) {
+                                //値の引き渡し
+                                //パスワード
+                                intent.putExtra("passwordKey", textPasswordEdit.getText().toString())
+                                //誕生年
+                                intent.putExtra("yearKey", textYearEdit.getText().toString())
+                                //誕生月
+                                intent.putExtra("monthKey", textMonthEdit.getText().toString())
+                                //誕生日
+                                intent.putExtra("dayKey", textDayEdit.getText().toString())
+                                startActivity(intent)
+                            }
+                        }
+                    }
+                }
             }
         }
 
         //戻るボタン
-        val backButton :ImageView = findViewById(R.id.backButton)
+        val backButton: ImageView = findViewById(R.id.backButton)
 
         //ログイン画面に遷移
         backButton.setOnClickListener {
-            val intent = Intent(this,loginScreen::class.java)
+            val intent = Intent(this, loginScreen::class.java)
             startActivity(intent)
         }
 
         //キャンセルボタン
-        val cancelButton : Button = findViewById(R.id.cancelButton)
+        val cancelButton: Button = findViewById(R.id.cancelButton)
 
         //ログイン画面に遷移
         cancelButton.setOnClickListener {
-            val intent = Intent(this,loginScreen::class.java)
+            val intent = Intent(this, loginScreen::class.java)
             startActivity(intent)
         }
+
     }
 }
