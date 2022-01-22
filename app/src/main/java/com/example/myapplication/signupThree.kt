9c94922f52173d490ae4e8fbfad424863ba41fb5
Hitominnn <libre.chat.1103@gmail.com>
2022-01-22 16:31:06 +0900

    update

--- app/src/main/java/com/example/myapplication/signupThree.kt
+++ app/src/main/java/com/example/myapplication/signupThree.kt
@@ -8,8 +8,18 @@ import android.widget.CheckBox
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.ListView
+import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
+import android.widget.AdapterView
 import android.view.Gravity
 import androidx.appcompat.app.AlertDialog
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.annotation.NonNull
+import androidx.annotation.LayoutRes
+
+
+
 
 //作成者：綾部，井口，久保田
 
@@ -49,21 +59,44 @@ class signupThree : AppCompatActivity() {
         val birthText =  findViewById<View>(R.id.birthday_edit_signupText) as TextView
         birthText.text = "  " + textYearEdit + "年" + textMonthEdit + "月" + textDayEdit + "日"
 
+
         //キーワード
         //改行されへんから追加された一番下しか表示できない
-        val favKeywordText = findViewById<View>(R.id.favKeywordText) as TextView
+//        val favKeywordText = findViewById<View>(R.id.favKeywordText) as TextView
+        var temp:Array<String?> = arrayOf("りんご", "りんご2")
+            // xmlにて実装したListViewの取得
+            val favKeywordView = findViewById<ListView>(R.id.favKeywordText)
+            // ArrayAdapterの生成
+            val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, temp)
+            // ListViewに、生成したAdapterを設定
+            favKeywordView.adapter = adapter
+
+
 //        favKeywordText.text = favKeyword!![0] +  "，" + favKeyword!![1] +  "，" + favKeyword!![2] +  "，" + favKeyword!![3] +  "，" + favKeyword!![4] +  "，" + favKeyword!![5] +  "，" + favKeyword!![6] +  "，" + favKeyword!![7] +  "，" + favKeyword!![8] +  "，" + favKeyword!![9]
-        if (favKeyword != null) {
-            for (i in 0..9){
-                if (favKeyword[i] == null){
-                }else{
-                    for(j in 0..i) {
-                        favKeywordText.text = "  \n"
-                    }
-                        favKeywordText.text = favKeyword[i]
-                }
-            }
-        }
+//        if (favKeyword != null) {
+//            for (i in 0..9){
+//                if (favKeyword[i] == null){
+//                }else{
+//                    for(j in 0..i) {
+//                        favKeywordText.text = "  \n"
+//                    }
+//                        favKeywordText.text = favKeyword[i]
+//                }
+//            }
+//        }
+
+
+//        if (favKeyword != null) {
+////            for (i in 0..9){
+////                if (favKeyword[i] == null){
+////                }else{
+//                    for(j in favKeyword.indices) {
+//                        favKeywordText.text = favKeyword[j]
+//                    }
+////                }
+////            }
+//        }
+
 
         //完了ボタン
         val completeButton: Button = findViewById(R.id.okButton)
@@ -92,15 +125,6 @@ class signupThree : AppCompatActivity() {
         }
 
 
-        //戻るボタン
-        val backButton : ImageView = findViewById(R.id.backButton)
-
-        //新規登録画面2に遷移
-        backButton.setOnClickListener {
-            val intent = Intent(this,signupTwo::class.java)
-            startActivity(intent)
-        }
-
         //キャンセルボタン
         val cancelButton : Button = findViewById(R.id.cancelButton)
 
@@ -111,7 +135,6 @@ class signupThree : AppCompatActivity() {
         }
 
     }
-
 
 
     var checkBoxState = arrayOf(0)
